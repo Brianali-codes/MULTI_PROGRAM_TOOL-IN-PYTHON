81ca1ec24eaf5a4f2501f3304bf86305149af8f0
gamerfunky78 <133756580+gamerfunky78@users.noreply.github.com>
2024-04-27 20:51:23 +0300

    Update simple_interest.py
    added new options

--- simple_interest.py
+++ simple_interest.py
@@ -1,9 +1,10 @@
 print("1:Hello there? ")
-print("2:This programme was made to calculate simple and compound interest")
+print("2:This programme was made aimimng to understand python basics.")
 print("____________________________________________________________________")
 print("1. simple interest.")
 print("2. compound interest.")
 print("3. quadratic equation.")
+print("4. Riddle Game.")
 print("_____________________________________________________________________")
 choice = input("input choice from above: ")
 
@@ -68,10 +69,57 @@ if choice == "3":
     print("the formulae = ax^2 + bx + c")
     print("therefore we substitute a b and c")
     print("________________________________________________________")
-    print("this becomes " + str(a)+"x^2 + " + str(b) + "x" + str(c))
-    print("the quadratic equation is -b ± √b -4ac /2a")
-    print("therefore the substitution becomes"+ str(-b) + "±" + str(b) + "-" + str(4*a*c) + "/" + str(2*a))
+    print("this becomes " + str(a)+" x^2 + " + str(b) + " x " + str(c))
+    print("")
+    print("the quadratic equation is {-b ± √b -4ac /2a}")
+    print("")
+    print("therefore the substitution becomes"+ str(-b) + " ± " + str(b) + " - " + str(4*a*c) + " / " + str(2*a))
+    print("")
+    print("")
     print(" x is therefore equal to:" + str(result1) + " or " + str(result2))
     print("_________________________________________________________")
     print("thanks for using this programme.")
     print("_________________________________________________________")
+if choice == "4":
+    print("RIDDLE GAME! ")
+    secret_word = "egg"
+    guess = ""
+    while guess != secret_word:
+        guess = input("my house has no doors: ")
+    print("well done you got it!    its an egg")
+
+    print("_________________________________________________________")
+    print("next riddle: ")
+    next_secret_word = "tortoise"
+    guess = ""
+    while guess != next_secret_word:
+        guess = input("am slow as f*ck: ")
+    print("well done you got it!    its a tortoise!!")
+
+
+    print("Well done you have found an easter egg!: ")
+    next_secret_word = "egg"
+    guess = ""
+    guess_count = 0
+    guess_limit = 3
+    out_of_guesses = False
+    while guess != next_secret_word:
+
+        if guess_count < guess_limit:
+            guess = input("input password to get source code, you have 3 attempts!: ")
+            guess_count += 1
+        else:
+          out_of_guesses = True
+
+    if out_of_guesses:
+     print("sorry better luck next time bro: ")
+    else :
+     print("nice here you go: ")
+     print("bruuhhhhhhhhh(ssds)sd90s)(((((ds0d9s09ds09d09s09d09s09d09s0n()SJDNJSKBKJDBJKJ  happy now")
+
+
+
+if choice == "29":
+    print("Well done you just found my favorite number, you are deemed worthy of taking on my project"
+          "here is the source code my brethren:")
+
