14d80db8f570b193d9ab433821ef7ee89919c8be
gamerfunky78 <133756580+gamerfunky78@users.noreply.github.com>
2024-05-02 10:54:53 +0300

    Update simple_interest.py

--- simple_interest.py
+++ simple_interest.py
@@ -14,7 +14,7 @@ choice = input("input choice from above: ")
 if choice == "1":
     print("________________________________________________________________")
     print("you chose simple interest: ")
-
+    # asks user to input different values related to their simple interest calculation
     deposit = input("input the deposit here: ")
     rate = input("input the interest rate here: ")
     time = input("input the time in years here: ")
@@ -30,7 +30,7 @@ if choice == "1":
 if choice == "2":
     print("________________________________________________________________")
     print("you chose compound interest: ")
-
+    # asks for compound interest input values from the user.
     deposit = float(input("input the deposit here: "))
     rate = float(input("input the interest rate in percentage here: "))
     time = float(input("input the time in years here: "))
@@ -50,9 +50,11 @@ if choice == "3":
     a = float(input("input a here:"))
     b = float(input("input b here:"))
     c = float(input("input c here:"))
+    # Input coefficients.
     discriminant = b ** 2 - 4 * a * c
     result1 = (-b + math.sqrt(discriminant)) / (2 * a)
     result2 = (-b - math.sqrt(discriminant)) / (2 * a)
+    # calculates discriminants.
     print("_______________________________________________________________")
     print("the formulae = ax^2 + bx + c")
     print("therefore we substitute a b and c")
@@ -64,12 +66,16 @@ if choice == "3":
     print("therefore the substitution becomes " + str(-b) + " ± " + str(b) + "-" + str(4 * a * c) + " / " + str(2 * a))
     print("")
     print("")
-    print(" x is therefore equal to:" + str(result1) + " or " + str(result2))
+    print(" x is therefore equal to:" + str(round(result1, 2)) + " or " + str(round(result2, 2)))
     print("________________________________________________________________")
     print("thanks for using this programme.")
     print("________________________________________________________________")
+
 if choice == "4":
     print("RIDDLE GAME! ")
+    # the user has to guess correctly in order to match the secret word to the guess.
+    print("the riddle game consists of 10 riddles the programme may not\n be entirely accurate.")
+    print("riddle 1: ")
     secret_word = "feather"
     guess = ""
     while guess != secret_word:
@@ -78,6 +84,7 @@ if choice == "4":
     print("well done you got it! its a feather. ")
 
     print("_________________________________________________________")
+    print("")
     print("2nd riddle: ")
     next_secret_word = "river"
     guess = ""
@@ -87,6 +94,8 @@ if choice == "4":
                       "\nhas a bed but. never sleeps?: ")
         print("try again")
     print("well done you got it! its a river!!")
+
+    print("_________________________________________________________")
     print("third riddle")
     secret_word2 = "age"
     guess = ""
@@ -94,6 +103,80 @@ if choice == "4":
         guess = input("What goes up but never comes down? ")
         print("try again")
     print("well done you got it! its your age. ")
+
+    print("_________________________________________________________")
+
+    print("fourth riddle. ")
+    secret_word3 = "footsteps"
+    guess = ""
+    while guess != secret_word3:
+        guess = input("The more you take, the more\n you leave behind. What am I?")
+        print("try again")
+    print(" well done its your footsteps")
+
+    print("_________________________________________________________")
+
+    print("fifth riddle. ")
+    secret_word4 = "piano"
+    guess = ""
+    while guess != secret_word4:
+        guess = input("What has keys but can't open locks?")
+        print("try again")
+    print(" well done its a piano.")
+
+    print("_________________________________________________________")
+
+    print("sixth riddle. ")
+    secret_word5 = "penny"
+    guess = ""
+    while guess != secret_word5:
+        guess = input("What has a head, a tail, is brown, and has no legs?")
+        print("try again")
+    print(" well done its a penny.")
+
+    print("_________________________________________________________")
+
+    print("seventh riddle. ")
+    secret_word6 = "stamp"
+    guess = ""
+    while guess != secret_word6:
+        guess = input("What travels the world while staying on a corner")
+        print("try again")
+    print(" well done its a stamp.")
+
+    print("_________________________________________________________")
+
+    print("eighth riddle. ")
+    secret_word7 = "m"
+    guess = ""
+    while guess != secret_word7:
+        guess = input(" What comes once in a minute, \n"
+                      "twice in a moment, but never in a thousand years")
+        print("try again")
+    print(" well done its the letter m.")
+
+    print("_________________________________________________________")
+    print("ninth riddle")
+    secret_word8 = "bottle"
+    guess = ""
+    while guess != secret_word8:
+        guess = input("What has a neck but no head?")
+        print("try again")
+    print(" well done its a bottle.")
+
+    print("_________________________________________________________")
+
+    print("tenth riddle")
+    secret_word9 = "time"
+    guess = ""
+    while guess != secret_word9:
+        guess = input("The more you take, the more you leave behind. What am I?")
+        print("try again")
+    print(" well done its time.")
+
+    # if the user answers wrong then the programme loops and asks to try again.
+
+    print("_________________________________________________________")
 
 if choice == "29":
     print("Well done you just found my favorite number, "
@@ -110,11 +193,28 @@ if choice == "5":
             print("wrong!!!")
         else:
             print("correct!!!")
+
 if choice == "6":
     print("simple calculator")
     num_1 = float(input("Input your first number"))
     operator = input("input operator( x + / or + )")
-    num_2 = float(input("Input your first number"))
-    result4 = int(num_1)+int(operator)+int(num_2)
+    num_2 = float(input("Input your second number"))
 
-    print(result4)
+    results = None
+
+    if operator == "+":
+        results = num_1 + num_2
+    if operator == "-":
+        results = num_1 - num_2
+    if operator == "/":
+        results = num_1 / num_2
+    if operator == "*":
+        results = num_1 * num_2
+    if operator == '/' and num_2 == 0:
+        print("Error! Division by zero.")
+
+    if results is not None:
+        print("Result:", results)
+if choice == "7":
+    print("")
+    
